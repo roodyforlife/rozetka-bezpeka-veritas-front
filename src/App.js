4bb6c28892ee98a2aa831e33d747ddfe23590621
matvey <kolossmatveyka@gmail.com>
2024-12-03 14:16:53 +0200

    fix deplot bugs

--- src/App.js
+++ src/App.js
@@ -1,27 +1,8 @@
-import axios from "axios";
-import { useEffect } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Settings } from "./pages/Settings";
 
 
 function App() {
-  // const fetchData = async () => {
-  //   try {
-  //     const response = await axios.get('http://localhost:8080/feed.xml', {
-  //       headers: {
-  //         'Content-Type': 'application/xml',
-  //       },
-  //     });
-  //     console.log(response.data);
-  //   } catch (error) {
-  //     console.error('Ошибка при запросе:', error);
-  //   }
-  // };
-
-  // useEffect(() => {
-  //   fetchData()
-  // }, [])
-
   return (
     <BrowserRouter>
       <Routes>
