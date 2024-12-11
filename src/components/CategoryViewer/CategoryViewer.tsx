58e03760fefa74c0dbe328134d00e35ca6ce3d0a
matvey <kolossmatveyka@gmail.com>
2024-12-11 16:10:42 +0200

    fix

--- src/components/CategoryViewer/CategoryViewer.tsx
+++ src/components/CategoryViewer/CategoryViewer.tsx
@@ -22,7 +22,7 @@ export const CategoryViewer = ({fetchFunction, saveFunction, viewer}: IProps) =>
 
   useEffect(() => {
     fetchCategories()
-  }, [])
+  }, [viewer])
 
   const handleItemChange = (categoryItem: ICategoryItem) => {
     setCategories(categories.map((category) => {
